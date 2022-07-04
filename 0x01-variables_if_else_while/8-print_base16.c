1c88bc2c4f7c56b3baf5cdd366ae295de617850c
Umar1999 <umarnasirahmad1999@gmail.com>
2022-07-04 04:51:12 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 char ch;
 int n;
 
-for(n = 48; n <= 57; n++)
+for	(n = 48; n <= 57; n++)
 {
 
 putchar(n);
 }
-for(ch = 'a'; ch <= 'f'; ch++)
+for	(ch = 'a'; ch <= 'f'; ch++)
 {
 
 putchar(ch);
