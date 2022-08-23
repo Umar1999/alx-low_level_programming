248beef72008cbcfed1070f40b8b0ea2e85ba9f4
Umar1999 <umarnasirahmad1999@gmail.com>
2022-08-23 10:32:55 -0700

    task correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 /**
  * _strlen - finds the length of a string
@@ -12,11 +8,11 @@
  */
 size_t _strlen(char *str)
 {
-	size_t i;
+	size_t j;
 
-	for	(i = 0; str[i]; i++)
+	for (j = 0; str[j]; j++)
 		;
-	return (i);
+	return (j);
 }
 
 /**
